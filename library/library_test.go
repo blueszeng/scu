b5f4aced673bc144bead4919ac0a7ffd62da618f
mohuishou <1@lailin.xyz>
2018-01-30 16:59:43 +0800

    【完善】图书馆

---
+++ library/library_test.go
@@ -0,0 +1,63 @@
+package library
+
+import (
+	"log"
+	"testing"
+
+	"github.com/gocolly/colly"
+	"github.com/mohuishou/scu/test"
+)
+
+func TestNewLibrary(t *testing.T) {
+	type args struct {
+		studentID string
+		password  string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "登陆成功",
+			args: args{
+				studentID: test.StudentID,
+				password:  test.Password,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewLibrary(tt.args.studentID, tt.args.password)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewLibrary() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			log.Println(*got)
+		})
+	}
+}
+
+func TestLibrary_GetLoanAll(t *testing.T) {
+	lib, _ := NewLibrary(test.StudentID, test.Password)
+	type args struct {
+		URL string
+		c   *colly.Collector
+	}
+	tests := []struct {
+		name string
+		lib  *Library
+	}{
+		{
+			name: "获取借阅书籍",
+			lib:  lib,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lib.GetLoanAll()
+			log.Println(got)
+		})
+	}
+}
